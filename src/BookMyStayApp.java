4ca4098cf31a7faf9265ecd5fcfe57432da901eb
vikhyatjain0657-ctrl <vikhyatjain0657@gmail.com>
2026-03-16 09:29:10 +0530

    Use Case 2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,47 @@
+/**
+ * MAIN CLASS - UseCase2RoomInitialization
+ *
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * Description:
+ * This class demonstrates room initialization
+ * using simple domain variables before introducing
+ * centralized inventory management.
+ *
+ * Availability is represented using
+ * basic variables to highlight limitations.
+ *
+ * @version 2.1
+ */
 public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     *
+     * @param args Command-Line arguments
+     */
+    public static void main(String[] args) {
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        Room single = new SingleRoom();
+        Room doub = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        System.out.println("Double Room:");
+        doub.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+    }
 }
