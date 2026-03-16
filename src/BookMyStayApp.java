bdab42052b7220d7ec40169b5aff9237ba80c460
vikhyatjain0657-ctrl <vikhyatjain0657@gmail.com>
2026-03-16 10:13:50 +0530

    Use Case 3: Centralized Room Inventory Management

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,33 @@
+/**
+ * MAIN CLASS - UseCase3InventorySetup
+ *
+ * @version 3.1
+ */
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        RoomInventory inventory = new RoomInventory();
+
+        Room single = new SingleRoom();
+        Room doub = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        System.out.println("Hotel Room Inventory Status\n");
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: "
+                + inventory.getRoomAvailability().get("Single") + "\n");
+
+        System.out.println("Double Room:");
+        doub.displayRoomDetails();
+        System.out.println("Available Rooms: "
+                + inventory.getRoomAvailability().get("Double") + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: "
+                + inventory.getRoomAvailability().get("Suite"));
+    }
 }
---
+++ src/Room.java
@@ -0,0 +1,23 @@
+/**
+ * ABSTRACT CLASS - Room
+ *
+ * @version 3.0
+ */
+public abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
